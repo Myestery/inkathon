ff76bf14876f7ffd1dce1adb1256ee89a771ed32
Chiwetelu Johnpaul Chidera <49923152+Myestery@users.noreply.github.com>
2023-10-28 07:24:40 +0000

    npm preparation

--- bin/create-inkathon.js
+++ bin/create-inkathon.js
@@ -7,32 +7,176 @@ import figlet from "figlet";
 import inquirer from "inquirer";
 import { promisify } from "util";
 
-const execAsync = promisify(exec);
-const checkForPnPm = () => {
-  let exists = false;
+// const execAsync = promisify(exec);
+
+const checkForPnPm = async () => {
   const commandToCheck = "pnpm -v";
 
-  exec(commandToCheck, (error, stdout, stderr) => {
-    if (error) {
-      exists = false;
-    } else {
-      console.log("pnpm is installed. Version:", stdout);
-      exists = true;
-    }
+  return new Promise((resolve) => {
+    exec(commandToCheck, (error, stdout, stderr) => {
+      if (error) {
+        resolve(false);
+      } else {
+        resolve(true);
+      }
+    });
   });
-  return exists;
 };
-let inkathonName = "";
-inquirer
-  .prompt([
+
+const askAndInstallPnPm = async () => {
+  const answers = await inquirer.prompt([
     {
-      type: "input",
-      name: "name",
+      type: "confirm",
+      name: "install",
       message:
-        "What is the name of the inkathon? We will use this to create a folder for your inkathon.",
+        "Pnpn is required to install inkathon. Do you want to install pnpm?",
     },
-  ])
-  .then(async (answers) => {
-    inkathonName = answers.name;
-    console.log(checkForPnPm());
+  ]);
+
+  if (answers.install) {
+    console.log(chalk.blue("Installing pnpm..."));
+    const commandToInstall = "npm install -g pnpm";
+    return new Promise((resolve) => {
+      exec(commandToInstall, (error, stdout, stderr) => {
+        if (error) {
+          console.error(chalk.red("Error installing pnpm."), error);
+          resolve(false);
+        } else {
+          console.log(chalk.green("pnpm installed."));
+          resolve(true);
+        }
+      });
+    });
+  } else {
+    // console.log(chalk.red("pnpm not installed."));
+  }
+};
+
+const createInkathon = async (name) => {
+  // clone repo
+  console.log(chalk.blue("Creating inkathon " + name + "..."));
+  const commandToCreate = `git clone https://github.com/scio-labs/inkathon.git ${inkathonName}`;
+  return new Promise((resolve) => {
+    exec(commandToCreate, (error, stdout, stderr) => {
+      if (error) {
+        console.error(chalk.red("Error creating inkathon."), error);
+        resolve(false);
+      } else {
+        console.log(chalk.green("inkathon created."));
+        resolve(true);
+      }
+    });
   });
+};
+
+const askForInstallDependencies = async () => {
+  const answers = await inquirer.prompt([
+    {
+      type: "list",
+      name: "install",
+      message: "Install dependencies?",
+      choices: [
+        {
+          name: "PnPm",
+          checked: true,
+        },
+        {
+          name: "Yarn",
+          checked: false,
+        },
+        {
+          name: "Npm",
+          checked: false,
+        },
+        {
+          name: "No",
+          checked: false,
+        },
+      ],
+      default: "PnPm",
+    },
+  ]);
+
+  if (answers.install) {
+    // switch and install
+    switch (answers.install) {
+      case "PnPm":
+        await installDependencies("pnpm");
+        break;
+      case "Yarn":
+        await installDependencies("yarn");
+        break;
+      case "Npm":
+        await installDependencies("npm");
+        break;
+      default:
+        break;
+    }
+  }
+};
+
+const installDependencies = async (provider) => {
+  console.log(chalk.blue("Installing dependencies..."));
+  const commandToInstall = `cd ${inkathonName} && ${provider} install`;
+  return new Promise((resolve) => {
+    exec(commandToInstall, (error, stdout, stderr) => {
+      if (error) {
+        console.error(chalk.red("Error installing dependencies."), error);
+        resolve(false);
+      } else {
+        console.log(chalk.green("Dependencies installed."));
+        resolve(true);
+      }
+    });
+  });
+};
+
+let inkathonName = "";
+(async () => {
+  // check if the name is already set
+  if (process.argv.length > 2) {
+    inkathonName = process.argv[2];
+  } else {
+    const answers = await inquirer.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What's the inkathon name for the folder?",
+        default: "inkathon" + Math.floor(Math.random() * 1000),
+      },
+    ]);
+
+    inkathonName = answers.name;
+  }
+
+  const pnpnExists = await checkForPnPm();
+  if (!pnpnExists || true) {
+    await askAndInstallPnPm();
+  }
+
+  await createInkathon(inkathonName);
+  await askForInstallDependencies();
+
+  // display figlet
+  console.log(
+    chalk.yellow(figlet.textSync("Ink!", { horizontalLayout: "full" })),
+    chalk.yellow(figlet.textSync("ready", { horizontalLayout: "full" }))
+  );
+
+  // closing remarks
+  console.log(
+    chalk.blue(
+      "Now that you have your inkathon, you can start it by running the following commands:"
+    )
+  );
+  console.log(chalk.green("cd " + inkathonName));
+  console.log(chalk.green("pnpm run dev"));
+
+  // do you need help?
+  console.log(
+    chalk.blue(
+      "If you need help installing rust, please visit the substrate documentation at"
+    ),
+    chalk.green("https://docs.substrate.io/install")
+  );
+})();
---
+++ readme.md
@@ -0,0 +1,45 @@
+# Create Ink App
+
+![Ink Logo](https://inkjs.org/img/logo.png)
+
+`create-ink-app` is a command-line tool that helps you quickly bootstrap Ink applications. It sets up the basic structure of your project, allowing you to start coding right away.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [What's Included](#whats-included)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Installation
+
+Before using `create-ink-app`, make sure you have Node.js and npm installed on your system. You can install it globally using npm:
+
+````bash
+npm install -g @myestery/create-ink-app
+
+
+Alternatively, you can use npx to run it without installing it globally:
+
+```bash
+npx @myestery/create-ink-app my-app
+````
+
+## Usage
+
+To create a new Ink app, open your terminal and run:
+
+```bash
+npx @myestery/create-ink-app my-app
+```
+
+Replace my-ink-app with the name of your project. This command will create a new directory with the specified name and set up the initial project structure.
+
+## Contributing
+
+We welcome contributions to improve create-ink-app. If you find a bug or have a feature request, please open an issue on the GitHub repository.
+
+## License
+
+This project is licensed under the MIT License. See the LICENSE file for details.
